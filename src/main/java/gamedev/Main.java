ff08fb2dd21a0b09a247296a852357e36c66431a
Viktor Daniv <viktor.daniv@student.tuke.sk>
2025-10-18 20:20:43 +0200

    Update file Main.java

--- src/main/java/gamedev/Main.java
+++ src/main/java/gamedev/Main.java
@@ -1,21 +1,17 @@
 package gamedev;
 
 import gamedev.controller.GameController;
-import gamedev.model.Game;
-import gamedev.model.view.GameView;
+import gamedev.model.view.MainMenu;
 import javafx.application.Application;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-import java.util.Collections;
-
 public class Main extends Application {
+
     @Override
     public void start(Stage primaryStage) {
-        Game game = new Game(Collections.emptyList()); // создаём игру без уровней
-        GameController controller = new GameController(game); // передаём игру в контроллер
-        GameView view = new GameView(controller, primaryStage); // создаём View с контроллером и Stage
-        controller.setView(view); // связываем контроллер с View
-        controller.startGame(primaryStage); // запускаем игру
+        MainMenu mainMenu = new MainMenu(primaryStage);
+        mainMenu.show();
     }
 
     public static void main(String[] args) {
